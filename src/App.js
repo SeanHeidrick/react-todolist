3e688309889b75e1a4da2bf4b4d1f0492f16964b
SeanHeidrick <seanheidrick1@gmail.com>
2020-05-05 13:29:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addItems = (e) => {
+    e.preventDefault();
+    let newItem = { input, completed: false };
+    let itemsCopy = [...items];
+    itemsCopy.push(newItem);
+    setItems(itemsCopy);
+    console.log(items);
+  };
+
+  const deleteItem = (i) => {
+    let itemsCopy = [...items];
+    itemsCopy.splice(i, 1);
+    setItems(itemsCopy);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>Todo List</h2>
+      <form onSubmit={addItems}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
+      <TodoList items={items} onDelete={deleteItem} />
     </div>
   );
 }
+
+const TodoList = ({ items, onDelete }) => {
+  return (
+    <ul>
+      {items.map((item, i) => {
+        return (
+          <li className="list-item">
+            <p>{item.input}</p>
+            <button onClick={() => onDelete(i)}>x</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default App;
